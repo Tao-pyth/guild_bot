47a73f2db9865fe4ec58f0abeccc05e9b0dc0365
Tao-pyth <54444867+Tao-pyth@users.noreply.github.com>
2019-08-24 19:49:32 +0900

    Commit
    テスト

--- testCD/test.py
+++ testCD/test.py
@@ -3,12 +3,17 @@ test.py テスト用　pythonファイル　
 2019/08/24
     1. xml使用テスト 2019/08/24
         20190824.xml
+    2. __name__の挙動　2019/08/24
 """
 #--1--
 import xml.etree.ElementTree as ET
+import test_fanc as T
+import sys
+sys.path.append("D:\\_user_template_\\Documents\\PG\\BOT\\guild_bot\\sys_fanc")
+import LOG_orig
 
 
-#--1--
+T.test_time(0)
 """
 ET.fromstring   <- XML形式のデータの読み込み関数
 ET.parse        <- XMLファイルを解析
@@ -23,3 +28,13 @@ for name in root.iter('name'):
     print(name.text)
 print(root[0].text)
 print(root[1].attrib)
+
+T.test_time(1)
+T.name()
+
+T.test_time(2)
+LOG = LOG_orig.LOG(__file__)
+LOG.msg("本日はお日柄もくよくよ")
+LOG.msg("本日はお日柄もくよくよ",2)
+LOG.msg("本日はお日柄もくよくよ",3)
+LOG.msg("本日はお日柄もくよくよ",6)
